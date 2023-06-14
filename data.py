40f9419f3db2f47a4599dcb0d81f8da9ad5c066b
rafaeloledo <rafaeloliveiraledo@gmail.com>
2023-06-14 14:55:48 -0300

    chore(py): update data, requirements and deps

--- data.py
+++ data.py
@@ -1,7 +1,12 @@
+import gi
+gi.require_version('Gtk', '3.0')
+
 import pandas as pd
 import numpy as np
 import seaborn as sbn
-import matplotlib as plt
+import matplotlib
+matplotlib.use('GTK3Agg')
+import matplotlib.pyplot as plt
 
 dic = { 
     "coluna 1": ['linha1', 'linha2', 'linha3'],
@@ -14,25 +19,21 @@ table = pd.DataFrame(dic)
 df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/alcohol-consumption/drinks.csv')
 
 #print(df.head())
-
 #print(df.shape)
-
 #print(df.info)
-
 #print(df.describe())
-
 #print(df.isnull().sum())
-
 #print(df.isnull().sum() / len(df) * 100)
-
-#print(df['country'])
-
 #print(df['country'][:10])
-
 #print(df['beer_servings'].value_counts())
-
 #print(df['wine_servings'].value_counts())
-
 #print(df['wine_servings'].value_counts().sort_values())
-
 #print(df[df['country'] == 'Brazil'])
+#print(df['total_litres_of_pure_alcohol'])
+
+consumo = df[df['total_litres_of_pure_alcohol'] >= 7] 
+sbn.countplot(data=consumo)
+#plt.ylabel('Porcentagem de consumo')
+#plt.show()
+#print(consumo.head(10))
+#print(plt.ylabel('Porcentagem de consumo'))
